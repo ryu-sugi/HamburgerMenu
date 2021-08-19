533a103a1e18c2e25b97ae245c36cff9b9e86a2d
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-08-19 15:25:36 +0900

    jsﾌｧｲﾙ作成, ｸﾘｯｸｲﾍﾞﾝﾄの設定

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// エラーチェック厳密化の記述
+'use strict'
+
+{
+ // HTMLからIDで取得してくる為の記述
+ const open = document.getElementById('open')
+
+ // querySelectorを使用する事でCSSセレクタを取得してくる
+ const overlay = document.querySelector('.overlay')
+
+ // クリックする事でイベント発火させる為の記述
+ open.addEventListener('click', () => {
+
+ });
+}
