a2d1b94f9a28cc3f03f751e4685971b3db2dd82c
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-08-20 13:10:48 +0900

    ﾒﾆｭｰを閉じる為の記述

--- js/main.js
+++ js/main.js
@@ -3,14 +3,23 @@
 
 {
  // HTMLからIDで取得してくる為の記述
- const open = document.getElementById('open')
+ const open = document.getElementById('open');
 
  // querySelectorを使用する事でCSSセレクタを取得してくる
- const overlay = document.querySelector('.overlay')
+ const overlay = document.querySelector('.overlay');
+
+ // HTMLから'close'IDを取得してくる
+ const close = document.getElementById('close');
 
  // クリックする事でイベント発火させる為の記述
  open.addEventListener('click', () => {
    overlay.classList.add('show'); //overlayのshowクラスを指定
    open.classList.add('hide'); //openのhideクラスを指定
  });
+
+ open.addEventListener('click', () => {
+  overlay.classList.remove('show'); 
+  open.classList.remove('hide'); 
+});
+
 }
